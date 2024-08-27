06d2b734256098e1ca9969c380663037447b6bc3
SimonHYQ <cellvsu@qq.com>
2024-08-27 09:18:30 +0000

    恢复代码

--- store/migrator.go
+++ store/migrator.go
@@ -147,8 +147,10 @@ func (s *Store) preMigrate(ctx context.Context) error {
 			return errors.Wrap(err, "failed to upsert migration history")
 		}
 	}
-	if err := s.normalizedMigrationHistoryList(ctx); err != nil {
-		return errors.Wrap(err, "failed to normalize migration history list")
+	if s.Profile.Mode == "prod" {
+		if err := s.normalizedMigrationHistoryList(ctx); err != nil {
+			return errors.Wrap(err, "failed to normalize migration history list")
+		}
 	}
 	return nil
 }
